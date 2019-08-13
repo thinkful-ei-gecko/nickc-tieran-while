367696c9c9aad7401009f33a2c640e944a4afacd
Tieran Dysart <tierandysart@gmail.com>
2019-08-13 12:19:55 -0600

    drills finished

--- while.js
+++ while.js
@@ -7,9 +7,32 @@ while (i <nums.length) {
         highest= nums[i]
     }
     i++;
-    
-
 }
 return highest;
 }
 
+function min(nums){
+    let lowest = 0;
+    let i = 0;
+    while(i < nums.length){
+        if(nums[i] < lowest){
+            lowest = nums[i]
+        }
+        i++
+    } return lowest
+}
+
+
+
+
+let testScores = [87, 88,65, 97];
+function avg (num){
+	let total = 0;
+	num.forEach(numbers => {
+		total += numbers;
+	});
+	return total / num.length;
+}
+
+avg(testScores);
+}
